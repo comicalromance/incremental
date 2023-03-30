5d4e458ccdbe4d467732eff92332762eeffbdcf5
comicalromance <silasyeoshuenyu@gmail.com>
2023-03-30 16:51:31 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/incremental/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./incremental/src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
